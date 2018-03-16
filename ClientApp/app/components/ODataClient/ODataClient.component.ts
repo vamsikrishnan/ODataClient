947022ba5e072edc2ebb70acdaca4677fb6efd00
vamsikrishnan <vamsikrishna.nyayapati@hotmail.com>
2018-03-16 16:39:51 +0530

    Filter Results

--- ClientApp/app/components/ODataClient/ODataClient.component.ts
+++ ClientApp/app/components/ODataClient/ODataClient.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     selector: 'ODataClient',
     templateUrl: './ODataClient.component.html'
 })
 export class ODataClientComponent {
+    public forecasts: WeatherForecast[];
+    public filter:Number ;
+    public resultsLimit:Number;
+    public filterValue:Number;
+    public jResult:string;
+    
+    constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string){
+    }
+
+    QueryData(event:any){
+        var url = this.baseUrl+ 'api/ODataClient?results='+ this.resultsLimit+'&filter=' + this.filter+'&filterValue=' + this.filterValue;    
+        this.http.get(url).subscribe(result => {
+            this.jResult = result.json() ;
+        }, error => console.error(error));
+    };
+
+    };
+
+interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
--- Controllers/ODataClientController.cs
+++ Controllers/ODataClientController.cs
@@ -4,20 +4,60 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 
 namespace ODataClient.Controllers
 {
+    [Route("api/[controller]")]
     public class ODataClientController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
 
-        public IActionResult Error()
+        // public IActionResult Error()
+        // {
+        //     ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        //     return View();
+        // }
+
+        [HttpGet]
+        public async Task<IActionResult> QueryData(string results, string filter, string filterValue)
         {
-            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
-            return View();
+            if (string.IsNullOrEmpty(results) ||
+            string.IsNullOrEmpty(filter) || string.IsNullOrEmpty(filterValue))
+            {
+                throw new ArgumentException("Parameter incorrect");
+            }
+
+            int resultCount;
+            int filterVal;
+            if(!int.TryParse(results, out resultCount))
+            {
+                throw new ArgumentOutOfRangeException("results incorrect");
+            }
+            if(!int.TryParse(filterValue, out filterVal))
+            {
+                throw new ArgumentOutOfRangeException("filterValue incorrect");
+            }
+
+            var criteria = string.Empty;
+            
+            if(filter=="1")
+                criteria = "lt";
+            if(filter=="2")
+                criteria = "gt";
+
+            var baseUrl = "http://services.odata.org/v4/(S(34wtn2c0hkuk5ekg0pjr513b))/TripPinServiceRW/";
+            var url = baseUrl + "People?$top=" +resultCount + " & $select=FirstName, LastName & $filter=Trips/any(d:d/Budget "+ criteria +" "+filterValue +")";
+            HttpClient httpClient = new HttpClient();
+            var res = await httpClient.GetStringAsync(url);
+            return new JsonResult(res);
         }
     }
 }
